6dadf35aba744e1ab09904da3dd4e0c82dc92977
Aquarmity <62366478+Aquarmity@users.noreply.github.com>
2023-12-09 21:07:07 -0800

    Day 9 complete

---
+++ day9/partTwo.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <sstream>
+using std::vector;
+
+bool allZeroes(const vector<int>& v);
+
+int main() {
+    vector<vector<vector<int>>> sequences;
+    std::ifstream infile("puzzleInput.txt");
+    std::stringstream currentLine;
+    std::string line;
+    int total;
+
+    getline(infile, line);
+    while (infile) {
+        currentLine << line;
+        vector<int> origSequence;
+        while (!currentLine.eof()) {
+            int num;
+            currentLine >> num;
+            origSequence.push_back(num);
+        }
+        currentLine.clear();
+
+        vector<vector<int>> sequenceSubset;
+        sequenceSubset.emplace_back(origSequence);
+        sequences.emplace_back(sequenceSubset);
+
+        getline(infile, line);
+    }
+
+    for (vector<vector<vector<int>>>::iterator i = sequences.begin(); i != sequences.end(); i++) {
+        for (int j = 0; j < i->size(); j++) {
+            vector<int> nextIteration;
+            int newSequenceTerm;
+            
+            for (vector<int>::iterator k = i->at(j).begin() + 1; k != i->at(j).end(); k++) {
+                newSequenceTerm = *k - *(k - 1);
+                nextIteration.push_back(newSequenceTerm);
+            }
+
+            i->emplace_back(nextIteration);
+            if (allZeroes(nextIteration)) { break; }
+        }
+    }
+
+    for (vector<vector<vector<int>>>::iterator i = sequences.begin(); i != sequences.end(); i++) {
+        int value;
+        for (vector<vector<int>>::iterator j = i->end() - 1; j != i->begin(); j--) {
+            (j - 1)->insert((j - 1)->begin(), ((j - 1)->front()) - j->front());
+            value = (j - 1)->front();
+        }
+
+        total += value;
+    }
+
+    std::cout << total;
+
+    return 0;
+}
+
+bool allZeroes(const vector<int>& v) {
+    for (int i : v) {
+        if (i != 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
