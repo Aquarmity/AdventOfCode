4a4b5471ff09f8ffc79a753444135595f4d300fb
Aquarmity <62366478+Aquarmity@users.noreply.github.com>
2023-12-11 13:56:44 -0800

    Day 11 complete

---
+++ day11/partOne.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+#include <utility>
+#include <cmath>
+
+const int GRID_SIZE = 140;
+
+int main() {
+    std::ifstream infile("puzzleInput.txt");
+    std::vector<std::pair<int, int>> galaxyPositions;
+    std::string line;
+    int currentLine = 0;
+    bool foundGalaxyOnLine = false;
+    int sum = 0;
+    bool emptyColumns[GRID_SIZE];
+    for (int i = 0; i < GRID_SIZE; i++) {
+        emptyColumns[i] = true;
+    }
+
+    getline(infile, line);
+    while (infile) {
+        for (int i = 0; i < line.length(); i++) {
+            if (line.at(i) == '#') {
+                emptyColumns[i] = false;
+                galaxyPositions.push_back(std::make_pair(currentLine, i));
+                foundGalaxyOnLine = true;
+            }
+        }
+
+        if (!foundGalaxyOnLine) { currentLine++; }
+        currentLine++;
+        foundGalaxyOnLine = false;
+        getline(infile, line);
+    }
+
+    for (int i = GRID_SIZE; i >= 0; i--) {
+        for (auto& g : galaxyPositions) {
+            if (emptyColumns[i] && g.second > i) {
+                g.second++;
+            }
+        }
+    }
+    for (auto i = galaxyPositions.begin(); i != galaxyPositions.end(); i++) {
+        for (auto j = i + 1; j != galaxyPositions.end(); j++) {
+            sum += abs(i->second - j->second) + abs(i->first - j->first);
+        }
+    }
+
+    std::cout << sum;
+}
