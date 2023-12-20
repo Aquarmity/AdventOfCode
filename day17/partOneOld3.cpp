020c05265c93047d7ab7c6b2d6878aa68ada9e05
Aquarmity <62366478+Aquarmity@users.noreply.github.com>
2023-12-20 00:10:41 -0800

    Day 17 complete
    finally
    Failed attempts of Part 1 included because it took me far too long

--- day17/partOneOld3.cpp
+++ day17/partOneOld3.cpp
@@ -1,60 +1,54 @@
 #include <iostream>
+#include <string>
 #include <fstream>
 #include <vector>
 #include <tuple>
 #include <queue>
+#include <algorithm>
 using std::get;
 
-const int GRID_SIZE = 141;
+const int GRID_SIZE = 13;
 const int MAX_STEPS = 3;
 const int NUM_DIRECTIONS = 4;
 
-// unused, but included for reference for corresponding directions
-// enum FacingDirection { UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3 };
+/* unused, but included for reference for corresponding directions
+enum FacingDirection { UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3, NODIRECTION = 4 };
+*/
 
 typedef std::tuple<int, int, int, int, int> quintuple;
 
 struct Block {
     int heatLoss;
     int heatLossFromStart;
+    //std::tuple<int, int, int, int> prevBlock;
     bool visited;
 
     Block() {
         heatLoss = 0;
         heatLossFromStart = INT_MAX;
+        //prevBlock = std::make_tuple(-1, -1, -1, -1);
         visited = false;
     }
 
     Block operator=(const Block& second) {
         heatLoss = second.heatLoss;
         heatLossFromStart = second.heatLossFromStart;
+        //prevBlock = second.prevBlock;
         visited = second.visited;
 
         return *this;
     }
 };
 
-int shortestPath(Block**** map);
-void enqueueNearbyBlocks(Block**** map, std::priority_queue<quintuple, std::vector<quintuple>, std::greater<quintuple>>& blockQueue,
+int shortestPath(Block map[GRID_SIZE][GRID_SIZE][MAX_STEPS][NUM_DIRECTIONS]);
+void enqueueNearbyBlocks(const Block map[GRID_SIZE][GRID_SIZE][MAX_STEPS][NUM_DIRECTIONS], std::priority_queue<quintuple, std::vector<quintuple>, std::greater<quintuple>>& blockQueue,
                          const std::tuple<int, int, int, int>& pos);
 
 int main() {
-    std::ifstream infile("puzzleInput.txt");
-
-    Block**** map = new Block***[GRID_SIZE];
-    for (int i = 0; i < GRID_SIZE; i++) {
-        map[i] = new Block**[GRID_SIZE];
-
-        for (int j = 0; j < GRID_SIZE; j++) {
-            map[i][j] = new Block*[MAX_STEPS];
-
-            for (int k = 0; k < MAX_STEPS; k++) {
-                map[i][j][k] = new Block[NUM_DIRECTIONS];
-            }
-        }
-    }
-
+    std::ifstream infile("test.txt");
+    Block map[GRID_SIZE][GRID_SIZE][MAX_STEPS][NUM_DIRECTIONS];
     char currentBlock;
+
     for (int i = 0; i < GRID_SIZE; i++) {
         for (int j = 0; j < GRID_SIZE; j++) {
             infile >> currentBlock;
@@ -71,9 +65,9 @@ int main() {
     return 0;
 }
 
-int shortestPath(Block**** map) {
+int shortestPath(Block map[GRID_SIZE][GRID_SIZE][MAX_STEPS][NUM_DIRECTIONS]) {
     std::priority_queue<quintuple, std::vector<quintuple>, std::greater<quintuple>> blocksToVisit;
-    std::tuple<int, int, int, int> curPos;
+    std::tuple<int, int, int, int> curPos(0, 0, 0, 0);
 
     map[0][0][0][0].heatLossFromStart = 0;
     for (int i = 0; i < GRID_SIZE; i++) {
@@ -92,7 +86,7 @@ int shortestPath(Block**** map) {
         Block* curBlock = &map[get<1>(curBlockInfo)][get<2>(curBlockInfo)][get<3>(curBlockInfo)][get<4>(curBlockInfo)];
         blocksToVisit.pop();
 
-        if (!curBlock->visited && curBlock->heatLossFromStart < INT_MAX) {
+        if (!curBlock->visited && curBlock->heatLossFromStart != INT_MAX) {
             std::priority_queue<quintuple, std::vector<quintuple>, std::greater<quintuple>> adjacentBlocks;
             curBlock->visited = true;
 
@@ -104,10 +98,15 @@ int shortestPath(Block**** map) {
                 Block* adjBlock = &map[get<1>(adjBlockInfo)][get<2>(adjBlockInfo)][get<3>(adjBlockInfo)][get<4>(adjBlockInfo)];
                 if (adjBlock->heatLossFromStart > curBlock->heatLossFromStart + adjBlock->heatLoss) {
                     adjBlock->heatLossFromStart = curBlock->heatLossFromStart + adjBlock->heatLoss;
+                    //adjBlock->prevBlock = curPos;
                     blocksToVisit.push(std::make_tuple(adjBlock->heatLossFromStart, get<1>(adjBlockInfo), get<2>(adjBlockInfo), get<3>(adjBlockInfo), get<4>(adjBlockInfo)));
                 }
             }
         }
+
+        /*if (curPos == std::make_pair(GRID_SIZE - 1, GRID_SIZE - 1)) {
+            break;
+        }*/
     }
 
     int leastHeatLoss = INT_MAX;
@@ -118,13 +117,13 @@ int shortestPath(Block**** map) {
             }
         }
     }
-    
+
     return leastHeatLoss;
 }
 
-void enqueueNearbyBlocks(Block**** map, std::priority_queue<quintuple, std::vector<quintuple>, std::greater<quintuple>>& blockQueue,
+void enqueueNearbyBlocks(const Block map[GRID_SIZE][GRID_SIZE][MAX_STEPS][NUM_DIRECTIONS], std::priority_queue<quintuple, std::vector<quintuple>, std::greater<quintuple>>& blockQueue,
                          const std::tuple<int, int, int, int>& pos) {
-    bool visitSameDirection = get<2>(pos) < MAX_STEPS - 1;
+    bool visitSameDirection = get<2>(pos) < 2;
 
     if (get<0>(pos) != 0 && get<3>(pos) != 1) {
         if (get<3>(pos) != 0) {
@@ -135,7 +134,7 @@ void enqueueNearbyBlocks(Block**** map, std::priority_queue<quintuple, std::vect
                             get<0>(pos) - 1, get<1>(pos), get<2>(pos) + 1, 0));
         }
     }
-    if (get<0>(pos) != GRID_SIZE - 1 && (get<3>(pos) != 0 || pos == std::make_tuple(0, 0, 0, 0))) {
+    if (get<0>(pos) != GRID_SIZE - 1 && get<3>(pos) != 0) {
         if (get<3>(pos) != 1) {
             blockQueue.push(std::make_tuple(map[get<0>(pos) + 1][get<1>(pos)][0][1].heatLossFromStart,
                             get<0>(pos) + 1, get<1>(pos), 0, 1));
---
+++ day17/partTwo.cpp
@@ -0,0 +1,170 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <tuple>
+#include <queue>
+using std::get;
+
+const int GRID_SIZE = 141;
+const int MAX_STEPS = 10;
+const int MIN_STEPS = 4;
+const int NUM_DIRECTIONS = 4;
+
+// unused, but included for reference for corresponding directions
+// enum FacingDirection { UP = 0, DOWN = 1, LEFT = 2, RIGHT = 3 };
+
+typedef std::tuple<int, int, int, int, int> quintuple;
+
+struct Block {
+    int heatLoss;
+    int heatLossFromStart;
+    std::tuple<int, int, int, int> prevBlock;
+    bool visited;
+
+    Block() {
+        heatLoss = 0;
+        heatLossFromStart = INT_MAX;
+        prevBlock = std::make_tuple(-1, -1, -1, -1);
+        visited = false;
+    }
+
+    Block operator=(const Block& second) {
+        heatLoss = second.heatLoss;
+        heatLossFromStart = second.heatLossFromStart;
+        prevBlock = second.prevBlock;
+        visited = second.visited;
+
+        return *this;
+    }
+};
+
+int shortestPath(Block**** map);
+void enqueueNearbyBlocks(Block**** map, std::priority_queue<quintuple, std::vector<quintuple>, std::greater<quintuple>>& blockQueue,
+                         const std::tuple<int, int, int, int>& pos);
+
+int main() {
+    std::ifstream infile("puzzleInput.txt");
+
+    Block**** map = new Block***[GRID_SIZE];
+    for (int i = 0; i < GRID_SIZE; i++) {
+        map[i] = new Block**[GRID_SIZE];
+
+        for (int j = 0; j < GRID_SIZE; j++) {
+            map[i][j] = new Block*[MAX_STEPS];
+
+            for (int k = 0; k < MAX_STEPS; k++) {
+                map[i][j][k] = new Block[NUM_DIRECTIONS];
+            }
+        }
+    }
+
+    char currentBlock;
+    for (int i = 0; i < GRID_SIZE; i++) {
+        for (int j = 0; j < GRID_SIZE; j++) {
+            infile >> currentBlock;
+            for (int k = 0; k < MAX_STEPS; k++) {
+                for (int w = 0; w < NUM_DIRECTIONS; w++) {
+                    map[i][j][k][w].heatLoss = currentBlock - '0';
+                }
+            }
+        }
+    }
+
+    std::cout << shortestPath(map);
+
+    return 0;
+}
+
+int shortestPath(Block**** map) {
+    std::priority_queue<quintuple, std::vector<quintuple>, std::greater<quintuple>> blocksToVisit;
+    std::tuple<int, int, int, int> curPos;
+
+    map[0][0][0][0].heatLossFromStart = 0;
+    for (int i = 0; i < GRID_SIZE; i++) {
+        for (int j = 0; j < GRID_SIZE; j++) {
+            for (int k = 0; k < MAX_STEPS; k++) {
+                for (int w = 0; w < NUM_DIRECTIONS; w++) {
+                    blocksToVisit.push(std::make_tuple(map[i][j][k][w].heatLossFromStart, i, j, k, w));
+                }
+            }
+        }
+    }
+
+    while (!blocksToVisit.empty()) {
+        quintuple curBlockInfo(blocksToVisit.top());
+        curPos = std::make_tuple(get<1>(curBlockInfo), get<2>(curBlockInfo), get<3>(curBlockInfo), get<4>(curBlockInfo));
+        Block* curBlock = &map[get<1>(curBlockInfo)][get<2>(curBlockInfo)][get<3>(curBlockInfo)][get<4>(curBlockInfo)];
+        blocksToVisit.pop();
+
+        if (!curBlock->visited && curBlock->heatLossFromStart < INT_MAX) {
+            std::priority_queue<quintuple, std::vector<quintuple>, std::greater<quintuple>> adjacentBlocks;
+            curBlock->visited = true;
+
+            enqueueNearbyBlocks(map, adjacentBlocks, curPos);
+
+            while (!adjacentBlocks.empty()) {
+                quintuple adjBlockInfo(adjacentBlocks.top());
+                adjacentBlocks.pop();
+                Block* adjBlock = &map[get<1>(adjBlockInfo)][get<2>(adjBlockInfo)][get<3>(adjBlockInfo)][get<4>(adjBlockInfo)];
+                if (adjBlock->heatLossFromStart > curBlock->heatLossFromStart + adjBlock->heatLoss) {
+                    adjBlock->heatLossFromStart = curBlock->heatLossFromStart + adjBlock->heatLoss;
+                    adjBlock->prevBlock = curPos;
+                    blocksToVisit.push(std::make_tuple(adjBlock->heatLossFromStart, get<1>(adjBlockInfo), get<2>(adjBlockInfo), get<3>(adjBlockInfo), get<4>(adjBlockInfo)));
+                }
+            }
+        }
+    }
+
+    int leastHeatLoss = INT_MAX;
+    for (int k = 0; k < MAX_STEPS; k++) {
+        for (int w = 0; w < NUM_DIRECTIONS; w++) {
+            if (map[GRID_SIZE - 1][GRID_SIZE - 1][k][w].heatLossFromStart < leastHeatLoss) {
+                leastHeatLoss = map[GRID_SIZE - 1][GRID_SIZE - 1][k][w].heatLossFromStart;
+            }
+        }
+    }
+
+    return leastHeatLoss;
+}
+
+void enqueueNearbyBlocks(Block**** map, std::priority_queue<quintuple, std::vector<quintuple>, std::greater<quintuple>>& blockQueue,
+                         const std::tuple<int, int, int, int>& pos) {
+    bool visitSameDirection = get<2>(pos) < MAX_STEPS - 1;
+
+    if (get<0>(pos) != 0 && get<3>(pos) != 1) {
+        if (get<3>(pos) != 0 && get<2>(pos) >= MIN_STEPS - 1) {
+            blockQueue.push(std::make_tuple(map[get<0>(pos) - 1][get<1>(pos)][0][0].heatLossFromStart,
+                            get<0>(pos) - 1, get<1>(pos), 0, 0));
+        } else if (visitSameDirection && get<3>(pos) == 0) {
+            blockQueue.push(std::make_tuple(map[get<0>(pos) - 1][get<1>(pos)][get<2>(pos) + 1][0].heatLossFromStart,
+                            get<0>(pos) - 1, get<1>(pos), get<2>(pos) + 1, 0));
+        }
+    }
+    if (get<0>(pos) != GRID_SIZE - 1 && (get<3>(pos) != 0 || pos == std::make_tuple(0, 0, 0, 0))) {
+        if (pos == std::make_tuple(0, 0, 0, 0) || (get<3>(pos) != 1 && get<2>(pos) >= MIN_STEPS - 1)) {
+            blockQueue.push(std::make_tuple(map[get<0>(pos) + 1][get<1>(pos)][0][1].heatLossFromStart,
+                            get<0>(pos) + 1, get<1>(pos), 0, 1));
+        } else if (visitSameDirection && get<3>(pos) == 1) {
+            blockQueue.push(std::make_tuple(map[get<0>(pos) + 1][get<1>(pos)][get<2>(pos) + 1][1].heatLossFromStart,
+                            get<0>(pos) + 1, get<1>(pos), get<2>(pos) + 1, 1));
+        }
+    }
+    if (get<1>(pos) != 0 && get<3>(pos) != 3) {
+        if (get<3>(pos) != 2 && get<2>(pos) >= MIN_STEPS - 1) {
+            blockQueue.push(std::make_tuple(map[get<0>(pos)][get<1>(pos) - 1][0][2].heatLossFromStart,
+                            get<0>(pos), get<1>(pos) - 1, 0, 2));
+        } else if (visitSameDirection && get<3>(pos) == 2) {
+            blockQueue.push(std::make_tuple(map[get<0>(pos)][get<1>(pos) - 1][get<2>(pos) + 1][2].heatLossFromStart,
+                            get<0>(pos), get<1>(pos) - 1, get<2>(pos) + 1, 2));
+        }
+    }
+    if (get<1>(pos) != GRID_SIZE - 1 && get<3>(pos) != 2) {
+        if (pos == std::make_tuple(0, 0, 0, 0) || (get<3>(pos) != 3 && get<2>(pos) >= MIN_STEPS - 1)) {
+            blockQueue.push(std::make_tuple(map[get<0>(pos)][get<1>(pos) + 1][0][3].heatLossFromStart,
+                            get<0>(pos), get<1>(pos) + 1, 0, 3));
+        } else if (visitSameDirection && get<3>(pos) == 3) {
+            blockQueue.push(std::make_tuple(map[get<0>(pos)][get<1>(pos) + 1][get<2>(pos) + 1][3].heatLossFromStart,
+                            get<0>(pos), get<1>(pos) + 1, get<2>(pos) + 1, 3));
+        }
+    }
+}
